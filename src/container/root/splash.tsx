7f23dd67df4b744dc4f510803f230a8a00442c09
inder <sameerhamid213@gmail.com>
2024-06-06 18:47:18 +0530

    added splash screen react native

--- src/container/root/splash.tsx
+++ src/container/root/splash.tsx
@@ -4,9 +4,11 @@ import {navigate} from '../../common/utils/NavigatorUtils';
 import {Image, View} from 'react-native';
 import {Images} from '../../common/constants/images';
 import {screenHeight, screenWidth} from '../../common/constants/dimensions';
+import SplashScreen from 'react-native-splash-screen';
 
 const Splash = () => {
   useEffect(() => {
+    SplashScreen.hide();
     setTimeout(() => {
       navigate(NavScreenTags.SIGNUP_SCREEN);
     }, 2000);
